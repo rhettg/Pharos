9f4358f7515087cbed1c9884ea074147169ff555
Rhett Garber <rhettg@gmail.com>
2010-09-24 19:20:48 -0700

    Better samples for public consumption

--- docs/config.py
+++ docs/config.py
@@ -2,5 +2,6 @@ import pharos
 
 metric_watchers = [
     pharos.CommandMetricWatcher(name="Is True?", command="true"),
-    pharos.PageGETMetricWatcher(name="home.ie from localhost", url="http://www.yelp.ie/", thresholds=(0.001, 0.800, 3.0))
+    pharos.CommandMetricWatcher(name="Is False?", command="false"),
+    pharos.PageGETMetricWatcher(name="Is Google Up", url="http://www.google.com/", thresholds=(0.001, 0.100, 1.0)),
 ]
