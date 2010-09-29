2b6bf75426b1c70e07d8e495d788279a85f3a2a3
Rhett Garber <rhett@yelp.com>
2010-09-29 10:13:24 -0700

    Reorganization to support proper packaging

---
+++ pharos/__init__.py
@@ -0,0 +1,6 @@
+from watcher import MetricWatcher
+from watcher import CommandMetricWatcher
+from watcher import PageGETMetricWatcher
+
+from web import MainHandler
+from web import PollMetricHandler
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
+import glob
 
 setup(
     name="Pharos", 
     version="0.0.1", 
     scripts=['bin/pharosd'],
-    package_dir={'':'lib'},
-    py_modules=['pharos']
+    packages=['pharos', 'pharos.views'],
+    package_data={'pharos.views': ['*.mustache']},
+    data_files=[
+                ('docs/pharos/', ['docs/config_sample.py']),
+                ('share/pharos/', glob.glob('static/*'))
+               ]
+    #py_modules=['pharos']
     # packages="pharos"
 )
